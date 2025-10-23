8af60c89dac9605c2af11f3025334c8a094af504
TheRealToky <tokydisaine.andrianarison@gmail.com>
2025-10-23 21:00:22 +0200

    fix 1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+sdfgfg
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    sdfvb
+    """
+    url = "https://www.reddit.com/r/{}/about".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+
+    response = requests.get(
+                url,
+                headers=headers,
+                allow_redirects=False
+                )
+    
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
