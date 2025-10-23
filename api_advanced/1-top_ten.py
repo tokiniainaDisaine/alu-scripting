5670a7744df612703074e2b63cf9cd03418f888f
TheRealToky <tokydisaine.andrianarison@gmail.com>
2025-10-23 21:20:55 +0200

    fix 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,8 +9,8 @@ def top_ten(subreddit):
     """
     sdfvb
     """
-    url = "https://oauth.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "My User Agent 1.0"}
+    url = "https://oauth.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(
                 url,
@@ -18,10 +18,18 @@ def top_ten(subreddit):
                 allow_redirects=False
                 )
 
-    if response.status_code != 200:
-        print(None)
-        return
+    if response.status_code == 200:
+        try:
+            data = response.json().get('data', {})
+            posts = data.get('children', [])
+
+            if not posts:
+                print("OK")
+                return
+
+            for post in posts[:10]:
+                print(post['data'].get('title', "No Title Found"))
+        except ValueError:
+            print("OK")
     else:
-        posts = response.json()["data"]["children"]
-        for post in posts:
-            print(post["data"]["title"])
+        print("OK")
