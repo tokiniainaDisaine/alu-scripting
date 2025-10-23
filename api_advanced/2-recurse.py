8d0389e7ae2ffce24fb52f619156526c5852e188
TheRealToky <tokydisaine.andrianarison@gmail.com>
2025-10-23 21:54:37 +0200

    fix 2

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+recursive
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    """
+    defghjn
+    """
+    subreddit_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Myapi-app'}
+    parameter = {'after': after}
+
+    response = requests.get(
+                subreddit_url,
+                headers=headers,
+                params=parameter,
+                allow_redirects=False)
+    
+    if response.status_code == 200:
+        datas = response.json()
+        values = datas['data']['children']
+
+        for each in values:
+            title = each['data']['title']
+            hot_list.append(title)
+        after = datas['data']['after']
+
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    return None
