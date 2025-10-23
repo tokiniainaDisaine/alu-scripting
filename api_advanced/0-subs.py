e731e137d16906ed1ace93d890fe2f7a768ed63a
TheRealToky <tokydisaine.andrianarison@gmail.com>
2025-10-23 20:38:46 +0200

    batch 0

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+wdefv
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    wedrfg
+    """
+    url = f"https://oauth.reddit.com/r/{str(subreddit)}/about"
+    headers = {"User-Agent": "Mozilla/5.0"}  
+
+    response = requests.get(
+                url, 
+                headers=headers, 
+                allow_redirects=False
+                )
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
